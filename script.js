4c4e16180a3467e4bd74d36486c7fcd2da075823
Jack Meier <jackson.meier423@gmail.com>
2020-06-15 22:23:24 -0700

    start button add, event delegation, timer functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+var timerEl = document.querySelector(".navbar-text");
+var startBtn = document.getElementById("start");
+// timerEl.textContent = "Time: "
+
+// might have to change function name later 
+function startTimer() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function () {
+        timerEl.textContent = timeLeft + " seconds remaining";
+        timeLeft--;
+
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            timerEl.textContent = "Time's Up!";
+        }
+    }, 1000);
+}
+
+startBtn.addEventListener("click", function () {
+
+    if (event.target.matches("button")) {
+        startTimer();
+    }
+});
